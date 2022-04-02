ec5682656ba5fd16c1ffa1f9f347fd588682b017
ANKITA <102715397+focused62@users.noreply.github.com>
2022-04-02 22:23:09 +0530

    VirusTurtle.py
    Python programming is used to Draw a Corona Virus .

---
+++ Virus.py
@@ -0,0 +1,17 @@
+from turtle import*
+import turtle
+speed(50)
+color('green')
+wn=turtle.Screen()
+wn.bgcolor('black')
+
+b=50
+
+while b < 200:
+
+	right( b)
+
+	forward(b * 3)
+
+	b = b + 1
+    
